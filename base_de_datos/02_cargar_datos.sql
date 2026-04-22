ba273695b5400daa9c1fa3d1373afa71b83ba798
Isaac van der Bom <isaacvanderbom4149@gmail.com>
2026-04-22 10:26:54 -0600

    XML TipoEvento cargado

--- base_de_datos/02_cargar_datos.sql
+++ base_de_datos/02_cargar_datos.sql
@@ -37,3 +37,38 @@ INSERT INTO Puesto (Nombre, SalarioxHora) VALUES('Albañil', 10.50)
 --Creo que hice mal esto, porque no sé si debe haber algún script que cargue esto automáticamente :,(  
 
 GO
+
+--Aqui vamos a insertar los tipos de evento
+/*
+TipoEvento Id="1" Nombre="Login Exitoso"/>
+<TipoEvento Id="2" Nombre="Login No Exitoso"/>
+<TipoEvento Id="3" Nombre="Login deshabilitado"/>
+<TipoEvento Id="4" Nombre="Logout"/>
+<TipoEvento Id="5" Nombre="Insercion no exitosa"/>
+<TipoEvento Id="6" Nombre="Insercion exitosa"/>
+<TipoEvento Id="7" Nombre="Update no exitoso"/>
+<TipoEvento Id="8" Nombre="Update exitoso"/>
+<TipoEvento Id="9" Nombre="Intento de borrado"/>
+<TipoEvento Id="10" Nombre="Borrado exitoso"/>
+<TipoEvento Id="11" Nombre="Consulta con filtro de nombre"/>
+<TipoEvento Id="12" Nombre="Consulta con filtro de cedula"/>
+<TipoEvento Id="13" Nombre="Intento de insertar movimiento"/>
+<TipoEvento Id="14" Nombre="Insertar movimiento exitoso"/>
+*/
+
+INSERT INTO TipoEvento(Id, Nombre) VALUES(1, 'Login Exitoso')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(2, 'Login No Exitoso')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(3, 'Login deshabilitado')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(4, 'Logout')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(5, 'Insercion no exitosa')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(6, 'Insercion exitosa')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(7, 'Update no exitoso')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(8, 'Update exitoso')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(9, 'Intento de borrado')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(10, 'Borrado exitoso')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(11, 'Consulta con filtro de nombre')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(12, 'Consulta con filtro de cedula')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(13, 'Intento de insertar movimiento')
+INSERT INTO TipoEvento(Id, Nombre) VALUES(14, 'Insertar movimiento exitoso')
+
+GO
