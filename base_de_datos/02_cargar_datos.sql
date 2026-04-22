2ca5a00da429dcfdcb6a5d03fe4dd5c908d8cb7f
Isaac Van de Bom <isaacvanderbom4149@gmail.com>
2026-04-22 14:31:34 -0600

    Listo cargar el xml, con este commit termino los movimientos

--- base_de_datos/02_cargar_datos.sql
+++ base_de_datos/02_cargar_datos.sql
@@ -192,3 +192,71 @@ INSERT INTO Empleado (IdPuesto, ValorDocumentoIdentidad, Nombre, FechaContrataci
 INSERT INTO Empleado (IdPuesto, ValorDocumentoIdentidad, Nombre, FechaContratacion) VALUES (
     (SELECT Id FROM Puesto WHERE Nombre = 'Niñera'),        '3389054', 'Sofía Herrera',    '2022-08-09');
 GO
+
+
+/*
+<movimiento ValorDocId="7517662" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-01-18" Monto="2" PostByUser="hardingmicheal" PostInIP="42.142.119.153" PostTime="2024-01-18 18:47:14"/>
+<movimiento ValorDocId="6993943" IdTipoMovimiento="Bono vacacional" Fecha="2024-10-31" Monto="1" PostByUser="mgarrison" PostInIP="156.92.82.57" PostTime="2024-10-31 12:43:18"/>
+<movimiento ValorDocId="8326328" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-11-22" Monto="7" PostByUser="andersondeborah" PostInIP="218.213.110.232" PostTime="2024-11-22 00:23:53"/>
+<movimiento ValorDocId="4510358" IdTipoMovimiento="Reversion de Credito" Fecha="2024-07-03" Monto="3" PostByUser="hardingmicheal" PostInIP="143.42.131.166" PostTime="2024-07-03 17:07:39"/>
+<movimiento ValorDocId="8403646" IdTipoMovimiento="Reversion de Credito" Fecha="2024-12-07" Monto="8" PostByUser="zkelly" PostInIP="155.44.100.105" PostTime="2024-12-07 15:44:30"/>
+<movimiento ValorDocId="8326328" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-11-26" Monto="10" PostByUser="hardingmicheal" PostInIP="141.163.255.56" PostTime="2024-11-26 09:33:41"/>
+<movimiento ValorDocId="6993943" IdTipoMovimiento="Disfrute de vacaciones" Fecha="2024-11-20" Monto="6" PostByUser="hardingmicheal" PostInIP="4.176.52.1" PostTime="2024-11-20 23:31:41"/>
+<movimiento ValorDocId="2918773" IdTipoMovimiento="Disfrute de vacaciones" Fecha="2024-10-30" Monto="10" PostByUser="zkelly" PostInIP="220.164.108.231" PostTime="2024-10-30 03:55:57"/>
+<movimiento ValorDocId="2161775" IdTipoMovimiento="Reversion Debito" Fecha="2024-06-13" Monto="2" PostByUser="hardingmicheal" PostInIP="135.223.57.22" PostTime="2024-06-13 13:28:39"/>
+<movimiento ValorDocId="8403646" IdTipoMovimiento="Bono vacacional" Fecha="2024-01-01" Monto="6" PostByUser="zkelly" PostInIP="150.250.94.62" PostTime="2024-01-01 05:17:10"/>
+<movimiento ValorDocId="2918773" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-07-12" Monto="6" PostByUser="hardingmicheal" PostInIP="218.191.123.15" PostTime="2024-07-12 09:10:16"/>
+<movimiento ValorDocId="5095109" IdTipoMovimiento="Reversion de Credito" Fecha="2024-12-27" Monto="14" PostByUser="hardingmicheal" PostInIP="136.103.23.170" PostTime="2024-12-27 12:59:03"/>
+<movimiento ValorDocId="6993943" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-04-08" Monto="1" PostByUser="jgonzalez" PostInIP="158.48.100.86" PostTime="2024-04-08 01:24:38"/>
+<movimiento ValorDocId="8403646" IdTipoMovimiento="Bono vacacional" Fecha="2024-08-25" Monto="8" PostByUser="jgonzalez" PostInIP="204.0.219.231" PostTime="2024-08-25 16:24:07"/>
+<movimiento ValorDocId="5095109" IdTipoMovimiento="Bono vacacional" Fecha="2024-03-07" Monto="7" PostByUser="andersondeborah" PostInIP="208.0.4.33" PostTime="2024-03-07 08:19:28"/>
+<movimiento ValorDocId="9772211" IdTipoMovimiento="Cumplir mes" Fecha="2024-02-14" Monto="4" PostByUser="martinezlisa" PostInIP="10.10.10.10" PostTime="2024-02-14 08:11:00"/>
+<movimiento ValorDocId="6641189" IdTipoMovimiento="Bono vacacional" Fecha="2024-02-28" Monto="3" PostByUser="floresdaniel" PostInIP="10.10.10.11" PostTime="2024-02-28 09:20:15"/>
+<movimiento ValorDocId="3389054" IdTipoMovimiento="Disfrute de vacaciones" Fecha="2024-03-12" Monto="5" PostByUser="perezmaria" PostInIP="10.10.10.12" PostTime="2024-03-12 14:05:45"/>
+<movimiento ValorDocId="9772211" IdTipoMovimiento="Reversion de Credito" Fecha="2024-04-03" Monto="2" PostByUser="torresluis" PostInIP="10.10.10.13" PostTime="2024-04-03 11:30:05"/>
+<movimiento ValorDocId="6641189" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-04-19" Monto="1" PostByUser="mgarrison" PostInIP="172.16.0.21" PostTime="2024-04-19 16:42:31"/>
+<movimiento ValorDocId="3389054" IdTipoMovimiento="Reversion Debito" Fecha="2024-05-02" Monto="3" PostByUser="jgonzalez" PostInIP="172.16.0.22" PostTime="2024-05-02 07:18:09"/>
+<movimiento ValorDocId="5095109" IdTipoMovimiento="Cumplir mes" Fecha="2024-05-18" Monto="6" PostByUser="andersondeborah" PostInIP="172.16.0.23" PostTime="2024-05-18 18:22:40"/>
+<movimiento ValorDocId="4510358" IdTipoMovimiento="Disfrute de vacaciones" Fecha="2024-06-09" Monto="4" PostByUser="hardingmicheal" PostInIP="172.16.0.24" PostTime="2024-06-09 12:10:55"/>
+<movimiento ValorDocId="6019592" IdTipoMovimiento="Bono vacacional" Fecha="2024-06-25" Monto="2" PostByUser="martinezlisa" PostInIP="172.16.0.25" PostTime="2024-06-25 09:44:03"/>
+<movimiento ValorDocId="7517662" IdTipoMovimiento="Reversion de Credito" Fecha="2024-07-11" Monto="5" PostByUser="floresdaniel" PostInIP="172.16.0.26" PostTime="2024-07-11 13:55:27"/>
+<movimiento ValorDocId="8403646" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-08-08" Monto="4" PostByUser="perezmaria" PostInIP="172.16.0.27" PostTime="2024-08-08 15:00:00"/>
+<movimiento ValorDocId="6993943" IdTipoMovimiento="Cumplir mes" Fecha="2024-09-14" Monto="7" PostByUser="torresluis" PostInIP="172.16.0.28" PostTime="2024-09-14 10:25:18"/>
+<movimiento ValorDocId="2161775" IdTipoMovimiento="Reversion Debito" Fecha="2024-10-05" Monto="1" PostByUser="zkelly" PostInIP="172.16.0.29" PostTime="2024-10-05 08:12:49"/>
+<movimiento ValorDocId="2918773" IdTipoMovimiento="Bono vacacional" Fecha="2024-11-03" Monto="2" PostByUser="martinezlisa" PostInIP="172.16.0.30" PostTime="2024-11-03 17:33:12"/>
+<movimiento ValorDocId="8326328" IdTipoMovimiento="Venta de vacaciones" Fecha="2024-12-18" Monto="8" PostByUser="floresdaniel" PostInIP="172.16.0.31" PostTime="2024-12-18 19:47:59"/>
+*/
+--Aqui vamos a insertar los movimientos aquí aprendí a que se puede poner como todo un un mismo paréntesis de value y se inserta todo
+
+INSERT INTO Movimiento (IdEmpleado, IdTipoMovimiento, Fecha, Monto, NuevoSaldo, IdPostByUser, PostInIP, PostTime) VALUES
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '7517662'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-01-18', 2, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '42.142.119.153', '2024-01-18 18:47:14'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6993943'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-10-31', 1, 0, (SELECT Id FROM Usuario WHERE Username = 'mgarrison'), '156.92.82.57', '2024-10-31 12:43:18'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8326328'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-11-22', 7, 0, (SELECT Id FROM Usuario WHERE Username = 'andersondeborah'), '218.213.110.232', '2024-11-22 00:23:53'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '4510358'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion de Credito'), '2024-07-03', 3, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '143.42.131.166', '2024-07-03 17:07:39'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8403646'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion de Credito'), '2024-12-07', 8, 0, (SELECT Id FROM Usuario WHERE Username = 'zkelly'), '155.44.100.105', '2024-12-07 15:44:30'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8326328'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-11-26', 10, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '141.163.255.56', '2024-11-26 09:33:41'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6993943'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Disfrute de vacaciones'), '2024-11-20', 6, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '4.176.52.1', '2024-11-20 23:31:41'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '2918773'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Disfrute de vacaciones'), '2024-10-30', 10, 0, (SELECT Id FROM Usuario WHERE Username = 'zkelly'), '220.164.108.231', '2024-10-30 03:55:57'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '2161775'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion Debito'), '2024-06-13', 2, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '135.223.57.22', '2024-06-13 13:28:39'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8403646'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-01-01', 6, 0, (SELECT Id FROM Usuario WHERE Username = 'zkelly'), '150.250.94.62', '2024-01-01 05:17:10'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '2918773'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-07-12', 6, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '218.191.123.15', '2024-07-12 09:10:16'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '5095109'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion de Credito'), '2024-12-27', 14, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '136.103.23.170', '2024-12-27 12:59:03'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6993943'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-04-08', 1, 0, (SELECT Id FROM Usuario WHERE Username = 'jgonzalez'), '158.48.100.86', '2024-04-08 01:24:38'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8403646'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-08-25', 8, 0, (SELECT Id FROM Usuario WHERE Username = 'jgonzalez'), '204.0.219.231', '2024-08-25 16:24:07'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '5095109'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-03-07', 7, 0, (SELECT Id FROM Usuario WHERE Username = 'andersondeborah'), '208.0.4.33', '2024-03-07 08:19:28'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '9772211'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Cumplir mes'), '2024-02-14', 4, 0, (SELECT Id FROM Usuario WHERE Username = 'martinezlisa'), '10.10.10.10', '2024-02-14 08:11:00'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6641189'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-02-28', 3, 0, (SELECT Id FROM Usuario WHERE Username = 'floresdaniel'), '10.10.10.11', '2024-02-28 09:20:15'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '3389054'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Disfrute de vacaciones'), '2024-03-12', 5, 0, (SELECT Id FROM Usuario WHERE Username = 'perezmaria'), '10.10.10.12', '2024-03-12 14:05:45'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '9772211'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion de Credito'), '2024-04-03', 2, 0, (SELECT Id FROM Usuario WHERE Username = 'torresluis'), '10.10.10.13', '2024-04-03 11:30:05'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6641189'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-04-19', 1, 0, (SELECT Id FROM Usuario WHERE Username = 'mgarrison'), '172.16.0.21', '2024-04-19 16:42:31'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '3389054'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion Debito'), '2024-05-02', 3, 0, (SELECT Id FROM Usuario WHERE Username = 'jgonzalez'), '172.16.0.22', '2024-05-02 07:18:09'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '5095109'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Cumplir mes'), '2024-05-18', 6, 0, (SELECT Id FROM Usuario WHERE Username = 'andersondeborah'), '172.16.0.23', '2024-05-18 18:22:40'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '4510358'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Disfrute de vacaciones'), '2024-06-09', 4, 0, (SELECT Id FROM Usuario WHERE Username = 'hardingmicheal'), '172.16.0.24', '2024-06-09 12:10:55'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6019592'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-06-25', 2, 0, (SELECT Id FROM Usuario WHERE Username = 'martinezlisa'), '172.16.0.25', '2024-06-25 09:44:03'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '7517662'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion de Credito'), '2024-07-11', 5, 0, (SELECT Id FROM Usuario WHERE Username = 'floresdaniel'), '172.16.0.26', '2024-07-11 13:55:27'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8403646'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-08-08', 4, 0, (SELECT Id FROM Usuario WHERE Username = 'perezmaria'), '172.16.0.27', '2024-08-08 15:00:00'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '6993943'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Cumplir mes'), '2024-09-14', 7, 0, (SELECT Id FROM Usuario WHERE Username = 'torresluis'), '172.16.0.28', '2024-09-14 10:25:18'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '2161775'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Reversion Debito'), '2024-10-05', 1, 0, (SELECT Id FROM Usuario WHERE Username = 'zkelly'), '172.16.0.29', '2024-10-05 08:12:49'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '2918773'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Bono vacacional'), '2024-11-03', 2, 0, (SELECT Id FROM Usuario WHERE Username = 'martinezlisa'), '172.16.0.30', '2024-11-03 17:33:12'),
+((SELECT Id FROM Empleado WHERE ValorDocumentoIdentidad = '8326328'), (SELECT Id FROM TipoMovimiento WHERE Nombre = 'Venta de vacaciones'), '2024-12-18', 8, 0, (SELECT Id FROM Usuario WHERE Username = 'floresdaniel'), '172.16.0.31', '2024-12-18 19:47:59');
+GO
