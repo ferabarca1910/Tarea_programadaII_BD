45d990857d82814fde7b7037870ee190c50e954c
Isaac Van de Bom <isaacvanderbom4149@gmail.com>
2026-04-23 08:08:57 -0600

    Parse Usuarios

--- base_de_datos/02_cargar_datos.sql
+++ base_de_datos/02_cargar_datos.sql
@@ -99,3 +99,27 @@ FROM @xml.nodes('/Error/error') AS T(Error);
 
 GO
 
+DECLARE @xml XML
+SET @xml = '<Usuarios>
+<usuario Id="1" Nombre="UsuarioScripts" Pass="UsuarioScripts"/>
+<usuario Id="2" Nombre="mgarrison" Pass=")*2LnSr^lk"/>
+<usuario Id="3" Nombre="jgonzalez" Pass="3YSI0HtiXI"/>
+<usuario Id="4" Nombre="zkelly" Pass="X4US4aLam@"/>
+<usuario Id="5" Nombre="andersondeborah" Pass="732F34xo%S"/>
+<usuario Id="6" Nombre="hardingmicheal" Pass="himB9Dzd%_"/>
+<usuario Id="7" Nombre="martinezlisa" Pass="7Kp9vQ2mT1"/>
+<usuario Id="8" Nombre="floresdaniel" Pass="H4s8Nq3xL6"/>
+<usuario Id="9" Nombre="perezmaria" Pass="R2m7Bv5cZ8"/>
+<usuario Id="10" Nombre="torresluis" Pass="J9t6Wk4pS3"/>
+</Usuarios>'
+
+INSERT INTO Usuario(Id, Username, [Password])
+SELECT 
+    Usuario.value('@Id','INT'),
+    Usuario.value('@Nombre','VARCHAR(100)'),
+    Usuario.value('@Pass','VARCHAR(100)')
+FROM @xml.nodes('/Usuarios/usuario') AS T(Usuario);
+
+GO
+
+
