391bdd37b4991bdbb75eb5232b7df378a3f6de47
Isaac Van de Bom <isaacvanderbom4149@gmail.com>
2026-04-23 08:00:20 -0600

    Feat: Parse del codigo del error

--- base_de_datos/02_cargar_datos.sql
+++ base_de_datos/02_cargar_datos.sql
@@ -76,3 +76,26 @@ FROM @xml.nodes('/TiposMovimientos/TipoMovimiento') AS T(TipoMovimiento);
 
 GO
 
+DECLARE @xml XML
+SET @xml = '<Error>
+<error Codigo="50001" Descripcion="Username no existe"/>
+<error Codigo="50002" Descripcion="Password no existe"/>
+<error Codigo="50003" Descripcion="Login deshabilitado"/>
+<error Codigo="50004" Descripcion="Empleado con ValorDocumentoIdentidad ya existe en inserción"/>
+<error Codigo="50005" Descripcion="Empleado con mismo nombre ya existe en inserción"/>
+<error Codigo="50006" Descripcion="Empleado con ValorDocumentoIdentidad ya existe en actualizacion"/>
+<error Codigo="50007" Descripcion="Empleado con mismo nombre ya existe en actualización"/>
+<error Codigo="50008" Descripcion="Error de base de datos"/>
+<error Codigo="50009" Descripcion="Nombre de empleado no alfabético"/>
+<error Codigo="50010" Descripcion="Valor de documento de identidad no alfabético"/>
+<error Codigo="50011" Descripcion="Monto del movimiento rechazado pues si se aplicar el saldo seria negativo."/>
+</Error>'
+
+INSERT INTO Error(Codigo, Descripcion)
+SELECT
+    Error.value('@Codigo','INT'),
+    Error.value('@Descripcion', 'VARCHAR(255))')
+FROM @xml.nodes('/Error/error') AS T(Error);
+
+GO
+
