da62251328558e535a98e2f3996fefac775b7072
Isaac Van de Bom <isaacvanderbom4149@gmail.com>
2026-04-23 09:00:56 -0600

    Parse empleados con errores

--- base_de_datos/02_cargar_datos.sql
+++ base_de_datos/02_cargar_datos.sql
@@ -94,7 +94,7 @@ SET @xml = '<Error>
 INSERT INTO Error(Codigo, Descripcion)
 SELECT
     Error.value('@Codigo','INT'),
-    Error.value('@Descripcion', 'VARCHAR(255))')
+    Error.value('@Descripcion', 'VARCHAR(255)')
 FROM @xml.nodes('/Error/error') AS T(Error);
 
 GO
@@ -122,4 +122,30 @@ FROM @xml.nodes('/Usuarios/usuario') AS T(Usuario);
 
 GO
 
+DECLARE @xml XML 
+SET @xml = '<Empleados>
+<empleado Puesto="Camarero" ValorDocumentoIdentidad="6993943" Nombre="Kaitlyn Jensen" FechaContratacion="2017-12-07"/>
+<empleado Puesto="Albañil" ValorDocumentoIdentidad="1896802" Nombre="Robert Buchanan" FechaContratacion="2020-09-20"/>
+<empleado Puesto="Cajero" ValorDocumentoIdentidad="5095109" Nombre="Christina Ward" FechaContratacion="2015-09-13"/>
+<empleado Puesto="Fontanero" ValorDocumentoIdentidad="8403646" Nombre="Bradley Wright" FechaContratacion="2020-01-27"/>
+<empleado Puesto="Conserje" ValorDocumentoIdentidad="6019592" Nombre="Robert Singh" FechaContratacion="2017-02-01"/>
+<empleado Puesto="Asistente" ValorDocumentoIdentidad="4510358" Nombre="Ryan Mitchell" FechaContratacion="2018-06-08"/>
+<empleado Puesto="Asistente" ValorDocumentoIdentidad="7517662" Nombre="Candace Fox" FechaContratacion="2013-12-17"/>
+<empleado Puesto="Asistente" ValorDocumentoIdentidad="8326328" Nombre="Allison Murillo" FechaContratacion="2020-04-19"/>
+<empleado Puesto="Cuidador" ValorDocumentoIdentidad="2161775" Nombre="Jessica Murphy" FechaContratacion="2017-04-12"/>
+<empleado Puesto="Fontanero" ValorDocumentoIdentidad="2918773" Nombre="Nancy Newton PhD" FechaContratacion="2016-11-22"/>
+<empleado Puesto="Conductor" ValorDocumentoIdentidad="9772211" Nombre="Alicia Ortega" FechaContratacion="2021-05-14"/>
+<empleado Puesto="Recepcionista" ValorDocumentoIdentidad="6641189" Nombre="Pedro Salas" FechaContratacion="2019-03-21"/>
+<empleado Puesto="Niñera" ValorDocumentoIdentidad="3389054" Nombre="Sofía Herrera" FechaContratacion="2022-08-09"/>
+</Empleados>'
 
+INSERT INTO Empleado(IdPuesto, ValorDocumentoIdentidad, Nombre, FechaContratacion)
+SELECT
+    (SELECT Id FROM Puesto WHERE Nombre = Empleado.value('@Puesto','VARCHAR(100)')),
+    Empleado.value('@ValorDocumentoIdentidad','VARCHAR(20)'),
+    Empleado.value('@Nombre','VARCHAR(150)'),
+    Empleado.value('@FechaContratacion','DATE')
+FROM @xml.nodes('/Empleados/empleado') AS T(Empleado);
+
+GO
+    
