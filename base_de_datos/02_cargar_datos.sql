dc94e50fc0c1e3cd6463d458c8d2ed2eb5e509ea
Isaac Van de Bom <isaacvanderbom4149@gmail.com>
2026-04-23 00:17:11 -0600

    Ahora sí, cargar desde xml a Puesto

--- base_de_datos/02_cargar_datos.sql
+++ base_de_datos/02_cargar_datos.sql
@@ -9,3 +9,24 @@
 USE ControlVacaciones;
 GO
 
+DECLARE @xml XML 
+SET @xml = '<Puestos>
+<Puesto Nombre="Cajero" SalarioxHora="11.00"/>
+<Puesto Nombre="Camarero" SalarioxHora="10.00"/>
+<Puesto Nombre="Cuidador" SalarioxHora="13.50"/>
+<Puesto Nombre="Conductor" SalarioxHora="15.00"/>
+<Puesto Nombre="Asistente" SalarioxHora="11.00"/>
+<Puesto Nombre="Recepcionista" SalarioxHora="12.00"/>
+<Puesto Nombre="Fontanero" SalarioxHora="13.00"/>
+<Puesto Nombre="Niñera" SalarioxHora="12.00"/>
+<Puesto Nombre="Conserje" SalarioxHora="11.00"/>
+<Puesto Nombre="Albañil" SalarioxHora="10.50"/>
+</Puestos>'
+
+INSERT INTO Puesto (Nombre, SalarioxHora)
+SELECT
+    Puesto.value('@Nombre', 'VARCHAR(100)'),
+    Puesto.value('@SalarioxHora', 'DECIMAL(10,2)') --No se porque pero no se puede poner el .value en CAPS porque no funka
+FROM @xml.nodes('/Puestos/Puesto') AS T(Puesto);
+
+GO
