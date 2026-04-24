56138bb1295fdfe5a40f7b539ba617c40308f5d1
ferabarca1910 <106549297+ferabarca1910@users.noreply.github.com>
2026-04-24 14:14:03 -0600

    Add files via upload
    En este documento trabajare el sp de login desde el Sql Server Management.

---
+++ SP_login.sql
@@ -0,0 +1,13 @@
+-- STORED PROCEDURE: sp_Login
+-- DESCRIPCION: Valida usuario y contrasena,
+--              revisa intentos fallidos en bitacora,
+--              deshabilita login si hay mas de 5 intentos
+--              en los ultimos 20 minutos
+
+CREATE PROCEDURE sp_Login
+
+AS
+
+BEGIN
+
+END
