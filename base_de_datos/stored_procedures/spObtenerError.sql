74551b10cdd24797a00249cfb4d989fb52f6b136
ferabarca1910 <106549297+ferabarca1910@users.noreply.github.com>
2026-04-28 17:35:36 -0600

    Update spObtenerError.sql
    listo obtener error, como se ve este no llevaba tanta complejidad y no es tan largo

--- base_de_datos/stored_procedures/spObtenerError.sql
+++ base_de_datos/stored_procedures/spObtenerError.sql
@@ -2,3 +2,43 @@
 -- DESCRIPCION: Recibe un codigo y retorna la
 --              descripcion del error desde la tabla Error.
 -- =============================================
+USE ControlVacaciones;
+GO
+
+CREATE PROCEDURE spObtenerError
+    -- IN
+    @Codigo      INT,
+    -- OUT
+    @CodigoError INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SET @CodigoError = 0;
+
+    BEGIN TRY
+
+        SELECT
+            Codigo,
+            Descripcion
+        FROM Error
+        WHERE Codigo = @Codigo;
+
+    END TRY
+    BEGIN CATCH
+        SET @CodigoError = 50008;
+
+        INSERT INTO DBError (UserName, Number, State, Severity, Line, [Procedure], Message, DateTime)
+        VALUES (
+            SYSTEM_USER,
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
+    END CATCH
+END
+GO
