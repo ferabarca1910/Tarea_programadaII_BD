f226adf86b8d320ba9d6da5647953e6bb0ea5021
ferabarca1910 <106549297+ferabarca1910@users.noreply.github.com>
2026-04-28 17:39:51 -0600

    Update spListarPuestos.sql
    igual que obtener error este es relativamente corto,ya quedo listo

--- base_de_datos/stored_procedures/spListarPuestos.sql
+++ base_de_datos/stored_procedures/spListarPuestos.sql
@@ -2,3 +2,39 @@
 -- DESCRIPCION: Retorna todos los puestos en orden
 --              alfabetico para llenar los dropdowns.
 -- =============================================
+CREATE PROCEDURE spListarPuestos
+    -- OUT
+    @CodigoError INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SET @CodigoError = 0;
+
+    BEGIN TRY
+
+        SELECT
+            Id,
+            Nombre,
+            SalarioxHora
+        FROM Puesto
+        ORDER BY Nombre ASC;
+
+    END TRY
+    BEGIN CATCH
+        SET @CodigoError = 50008;
+
+        INSERT INTO DBError (UserName, Number, State, Severity, Line, [Procedure], Message, DateTime)
+        VALUES (
+            SYSTEM_USER,
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
+    END CATCH
+END
+GO
