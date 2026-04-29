124fb76cfeff11aeb24e49592ff7b127edf2e75e
ferabarca1910 <106549297+ferabarca1910@users.noreply.github.com>
2026-04-28 18:03:54 -0600

    Update spLogout.sql
    listo el stored de logout

--- base_de_datos/stored_procedures/spLogout.sql
+++ base_de_datos/stored_procedures/spLogout.sql
@@ -2,3 +2,41 @@
 -- DESCRIPCION: Registra el cierre de sesion
 --              del usuario en la bitacora
 -- =============================================
+USE ControlVacaciones;
+GO
+
+CREATE PROCEDURE spLogout
+    -- IN
+    @IdUsuario   INT,
+    @IP          VARCHAR(50),
+    -- OUT
+    @CodigoError INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SET @CodigoError = 0;
+
+    BEGIN TRY
+
+        INSERT INTO BitacoraEvento (IdTipoEvento, Descripcion, IdPostByUser, PostInIP, PostTime)
+        VALUES (4, NULL, @IdUsuario, @IP, GETDATE());
+
+    END TRY
+    BEGIN CATCH
+        SET @CodigoError = 50008;
+
+        INSERT INTO DBError (UserName, Number, State, Severity, Line, [Procedure], Message, DateTime)
+        VALUES (
+            SYSTEM_USER,
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
+    END CATCH
+END
+GO
