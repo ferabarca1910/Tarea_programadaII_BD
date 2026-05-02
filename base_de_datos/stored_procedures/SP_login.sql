e1db537d18baff2c88922d15355c33e349dd1635
Isaac Van de Bom <isaacvanderbom4149@gmail.com>
2026-05-01 18:40:30 -0600

    Estaba mal la parte de ends y trys

--- base_de_datos/stored_procedures/SP_login.sql
+++ base_de_datos/stored_procedures/SP_login.sql
@@ -135,6 +135,21 @@ BEGIN
 
     INSERT INTO BitacoraEvento (IdTipoEvento, Descripcion, IdPostByUser, PostInIP, PostTime)
     VALUES (1, 'Exitoso', @IdUsuario, @IP, GETDATE());
+    END TRY
+    BEGIN CATCH
+        SET @CodigoError = 50008;
 
+        INSERT INTO DBError (UserName, Number, State, Severity, Line, [Procedure], Message, DateTime)
+        VALUES (
+            SYSTEM_USER,
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
+    END CATCH
 END
 GO
